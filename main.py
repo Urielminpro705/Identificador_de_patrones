468f18218b4eed61950dec5890c099cab1fe1064
Urielminpro705 <urielmata37@hotmail.com>
2024-10-30 15:29:54 -0600

    UWU

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import streamlit as st
 from skimage import io, color
+from skimage import img_as_float
+from utils.template_matching import template_matching
 import numpy as np
 
 st.title("Buscador de patrones")
-img_objetivo_uploader = st.file_uploader("Elige una imagen objeivo", type=['jpg', 'jpeg', 'png'])
-seccion_img_uploader = st.file_uploader("Elige la imagen a buscar", type=['jpg', 'jpeg', 'png'])
+img_objetivo_uploader = st.file_uploader("Elige una imagen objeivo", type=['jpg', 'jpeg','png'])
+seccion_img_uploader = st.file_uploader("Elige la imagen a buscar", type=['jpg', 'jpeg','png'])
 
 col1,col2 = st.columns([0.5,0.5], gap="small", vertical_alignment="top")
 
@@ -12,10 +14,16 @@ if img_objetivo_uploader is not None:
     img_objetivo = io.imread(img_objetivo_uploader)
     with col1:
         st.header("Donde buscar")
-        st.image(img_objetivo, caption='Imagen Objetivo')
+        st.image(img_objetivo, caption="Imagen Objetivo")
 
 if seccion_img_uploader is not None:
     seccion_img = io.imread(seccion_img_uploader)
     with col2:
         st.header("Que buscar")
-        st.image(seccion_img, caption='Seccion a buscar')
+        st.image(seccion_img, caption="Seccion a buscar")
+
+if img_objetivo_uploader is not None and seccion_img_uploader is not None:
+    response = template_matching(img_objetivo, seccion_img)
+    img_out = response.get("image")
+    st.header("Resultado")
+    st.image(img_out, caption="Resultado")
--- utils/template_matching.py
+++ utils/template_matching.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-def template_matching(img : np.array, smaller_img : np.array):
-    h, w, _ = img.shape
-    sh, sw, _ = smaller_img.shape
+def template_matching(img : np.array, smaller_img : np.array, color_marco = [255,0,0], grosor_marco = 5):
+    h, w, can = img.shape
+    sh, sw, can = smaller_img.shape
 
     if h < sh or w < sw:
         print("La imagen a buscar es mas pequeña que la imagen objetivo")
@@ -11,29 +11,47 @@ def template_matching(img : np.array, smaller_img : np.array):
         h_iteracion = h - sh + 1
         w_iteracion = w - sw + 1
         ssd = np.zeros((h_iteracion,w_iteracion))
-
+        # Valor, fila, columna
+        mas_parecido = [float("inf"),0,0]
         for f in range(h_iteracion):
             for c in range(w_iteracion):
                 seccion = img[f:f+sh, c:c+sw]
-                ssd[f,c] = np.sum((seccion - smaller_img)**2)
+                menor = np.sum((seccion - smaller_img)**2)
+                ssd[f,c] = menor
+                if menor < mas_parecido[0]:
+                    mas_parecido = [menor,f,c]
+                    if menor == 0:
+                        break
+        
+        _, f, c = mas_parecido
+        img_out = img[f:f+sh, c:c+sw, :]
+
+        # Top
+        img[f:f+grosor_marco, c:c+sw] = color_marco
+        # Bottom
+        img[f+sh-grosor_marco:f+sh, c:c+sw] = color_marco
+        # Start
+        img[f:f+sh, c:c+grosor_marco] = color_marco
+        # End
+        img[f:f+sh, c+sw-grosor_marco:c+sw] = color_marco
 
         response = {
-            "doesExist" : False,
-            "Image": None
+            "ssd": ssd,
+            "image": img
         }
-        print(ssd)
         return response
 
 
-img = np.array([
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-])
+# img = np.array([
+#     [[1,2,3],[4,5,6],[7,8,9]],
+#     [[10,11,12],[13,14,15],[16,17,18]],
+#     [[19,20,21],[22,23,24],[25,26,27]]
+# ])
 
-smaller_img = np.array([
-    [4,5],
-    [7,8]
-])
+# smaller_img = np.array([
+#     [[10,11,12],[13,14,15]],
+#     [[19,20,21],[22,23,24]]
+# ])
 
-template_matching(img, smaller_img)
+# response = template_matching(img, smaller_img)
+# print(response.get("image"))
