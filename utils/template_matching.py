532cf9455a821254e28040cab748927f03dbca29
Urielminpro705 <urielmata37@hotmail.com>
2024-11-15 14:11:39 -0600

    Se comenzó a implementar el patron de diseño template method para personalizar el algoritmo de template matching

--- utils/template_matching.py
+++ utils/template_matching.py
@@ -1,10 +1,106 @@
 import numpy as np
-from skimage import img_as_ubyte
 from skimage.color import rgb2gray
 from skimage.transform import resize
-import streamlit as st
+from abc import ABC, abstractmethod
+
+class Template_Matching(ABC):
+    def __init__(self):
+        self.img = None
+        self.smaller_img = None
+        self.img_grey = None
+        self.smaller_img_grey = None
+        self.h = None
+        self.w = None
+        self.sw = None
+        self.sh = None
+        self.progress_bar = None
+        self.optimized = False
+        self.new_size = 100
+        self.more_similar = None
+        self.cut_result = None
+        self.border_color = [255,0,0]
+        self.border = 5
+        pass
+    
+    def __grayscale(self):
+        if len(self.img.shape) > 2:
+            self.img_grey = rgb2gray(self.img)
+        else:
+            self.img_grey = self.img
+            self.img = np.stack((self.img,) * 3, axis = -1)
+        if len(self.smaller_img.shape) > 2:
+            self.smaller_img_grey = rgb2gray(self.smaller_img)
+        else:
+            self.smaller_img_grey = self.smaller_img
+            self.smaller_img = np.stack((self.smaller_img,) * 3, axis = -1)
+
+    def size_check(self):
+        self.h, self.w = self.img_grey.shape
+        self.sh, self.sw = self.smaller_img_grey.shape
+        if self.h < self.sh or self.w < self.sw:
+            print("La imagen a buscar es mas pequeña que la imagen objetivo")
+            self.progress_bar.empty()
+            return False
+        return True
+
+    def is_optimized(self):
+        if self.optimized:
+            cut_result = cut_img(self.smaller_img_grey, self.new_size)
+            self.sh = self.new_size if cut_result[2] != 0 else self.sh
+            self.sw = self.new_size if cut_result[1] != 0 else self.sw
+            self.smaller_img_grey = cut_result[0]
+
+    @abstractmethod
+    def sdc(self):
+        h_iteration = self.h - self.sh + 1
+        w_iteration = self.w - self.sw + 1
+        ssd = np.zeros((h_iteration,w_iteration))
+        total_iterations = h_iteration*w_iteration
+        update_interval = total_iterations // 10
+        if update_interval == 0:
+            update_interval = 1
+        completed_iterations = 0
+
+        # Valor, fila, columna
+        self.more_similar = [float("inf"),0,0]
+        for f in range(h_iteration):
+            for c in range(w_iteration):
+                section = self.img_grey[f:f+self.sh, c:c+self.sw]
+                value = np.sum((section - self.smaller_img_grey)**2)
+                ssd[f,c] = value
+                completed_iterations += 1
+                if self.progress_bar:
+                    if completed_iterations % update_interval == 0:
+                        progress_percentage = int((completed_iterations / total_iterations) * 100)
+                        self.progress_bar.progress(progress_percentage, "Buscando...")
+                if value < 0.4:
+                    more_similar = [value,f,c]
+                    break
+                if value < more_similar[0]:
+                    more_similar = [value,f,c]
+        
+        self.sh, self.sw, _ = self.smaller_img.shape
+    
+    def add_frame(self):
+        _, f, c = self.more_similar
+        if self.optimized:
+            f -= self.cut_result[2]
+            c -= self.cut_result[1]
+
+        # Top
+        self.img[f:f+self.border, c:c+self.sw] = self.border_color
+        # Bottom
+        self.img[f+self.sh-self.border:f+self.sh, c:c+self.sw] = self.border_color
+        # Start
+        self.img[f:f+self.sh, c:c+self.border] = self.border_color
+        # End
+        self.img[f:f+self.sh, c+self.sw-self.border:c+self.sw] = self.border_color
+        if self.progress_bar:
+            self.progress_bar.progress(100, "Completado")
+
 
 def template_matching(img : np.array, smaller_img : np.array, border_color = [255,0,0], border = 5, optimized = False, new_size = 100, progress_bar=None):
+    # Conversion a escala de grises
     if len(img.shape) > 2:
         img_grey = rgb2gray(img)
     else:
@@ -19,9 +115,10 @@ def template_matching(img : np.array, smaller_img : np.array, border_color = [25
     else:
         smaller_img_grey = smaller_img
         smaller_img = np.stack((smaller_img,) * 3, axis = -1)
+    
+    # Comprobar tamaño
     h, w = img_grey.shape
     sh, sw = smaller_img_grey.shape
-
     if h < sh or w < sw:
         print("La imagen a buscar es mas pequeña que la imagen objetivo")
         progress_bar.empty()
